271548eb499c83aabe170739309dfa7b264cccc4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-03 14:24:45 -0500

    Add ayah parsers

---
+++ src/util/parsers.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/ayaanqui/quran-explorer/src/types"
+)
+
+// Expected verse format: 114|6|مِنَ الْجِنَّةِ وَالنَّاسِ [۩]
+// Order: chapter number, verse number, ayah or verse, and the symbol ۩ indicating a sajdah.
+// Return: (chapter number, verse number, sajdah, verse)
+func ParseRawAyah(raw_verse string) (uint, uint, bool, string) {
+	parsed_ayah := strings.Split(raw_verse, "|")
+	if len(parsed_ayah) < 3 {
+		panic("Invalid ayah form: " + raw_verse)
+	}
+	
+	chapter_number, err := strconv.ParseUint(parsed_ayah[0], 10, 32)
+	if err != nil {
+		PanicMsg(err, "Could not covert chapter number it uint: " + raw_verse + ": " + parsed_ayah[2])
+	}
+
+	verse_number, err := strconv.ParseUint(parsed_ayah[1], 10, 32)
+	if err != nil {
+		PanicMsg(err, "Could not covert verse number it uint: " + raw_verse + ": " + parsed_ayah[1])
+	}
+	verse_raw := parsed_ayah[2]
+
+	return uint (chapter_number), uint (verse_number), false, verse_raw
+}
+
+func BuildAyah(raw_verse string) *types.Ayah {
+	ch_num, verse_num, sajdah, verse := ParseRawAyah(raw_verse)
+	return &types.Ayah{
+		VerseNumber: verse_num, 
+		ChapterNumber: ch_num, 
+		Sajdah: sajdah, 
+		Verse: verse,
+	}
+}
