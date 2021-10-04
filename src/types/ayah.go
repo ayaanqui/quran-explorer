1c38f20b7dd73ba9a981907ab404ddf08def3d88
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-03 23:30:29 -0500

    Add ToString method to Ayah

--- src/types/ayah.go
+++ src/types/ayah.go
@@ -1,8 +1,24 @@
 package types
 
+import (
+	"fmt"
+)
+
 type Ayah struct {
 	VerseNumber uint
 	ChapterNumber uint
 	Sajdah bool
 	Verse string
+}
+
+type IAyah interface {
+	ToString() string
+}
+
+func (a Ayah) ToString() string {
+	res := fmt.Sprintf("%d:%d %s", a.ChapterNumber, a.VerseNumber, a.Verse)
+	if a.Sajdah {
+		return res + " Sajdah"
+	}
+	return res
 }
